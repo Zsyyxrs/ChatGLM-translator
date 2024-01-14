ddb979774368540e956aae630f35a32b9393bf04
shangyi.zhu <zhusayi1994@gmail.com>
2024-01-14 23:20:01 +0800

    ChatGLM

---
+++ ChatGLM-translator/ai_translator/translator/__init__.py
@@ -0,0 +1,6 @@
+from .exceptions import PageOutOfRangeException
+from .pdf_parser import PDFParser
+from .pdf_translator import PDFTranslator
+from .translation_chain import TranslationChain
+from .translation_config import TranslationConfig
+from .writer import Writer
